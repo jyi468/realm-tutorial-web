6b4b9d0783c2692e628f76a8be737ffc10098227
jyiwm <joshua.yi@warnermedia.com>
2022-05-06 15:34:38 -0400

    Update auth files. Update env to prevent warnings

--- src/graphql/RealmApolloProvider.js
+++ src/graphql/RealmApolloProvider.js
@@ -19,8 +19,8 @@ const createRealmApolloClient = (app) => {
       }
       // Refreshing a user's custom data also refreshes their access token
       await app.currentUser.refreshCustomData();
-      // TODO: Include the current user's access token in an Authorization header with
-      // every request.
+      // The handler adds a bearer token Authorization header to the otherwise unchanged request
+      options.headers.Authorization = `Bearer ${app.currentUser.accessToken}`;
       return fetch(uri, options);
     },
   });
@@ -31,6 +31,11 @@ const createRealmApolloClient = (app) => {
 };
 
 export default function RealmApolloProvider({ children }) {
-  // TODO: Create an ``ApolloClient`` object that connects to your app.
+  const app = useRealmApp();
+  const [client, setClient] = React.useState(createRealmApolloClient(app));
+  React.useEffect(() => {
+    setClient(createRealmApolloClient(app));
+  }, [app]);
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
+
